4d3949b7b36aa7acd206efcbe02b550ae7586ad3
VladislavGolovatuik <golovatuik.vladislav@gmail.com>
2017-06-26 10:22:45 -0700

    Тест № 1

--- src/SmartGitTest.java
+++ src/SmartGitTest.java
@@ -1,7 +1,9 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Cvladislav on 26.06.2017.
  */
 public class SmartGitTest {
+    public static void main (String [] args) {
+        System.out.println("test # 1");
+
+    }
 }
